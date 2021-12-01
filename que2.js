602b54858f16b768c594f26523611950135d2360
coder biceps <manugupta619@gmail.com>
2021-12-01 19:00:51 +0530

    add regex to check valid email

---
+++ que2.js
@@ -0,0 +1,6 @@
+let regex = /^([a-zA-Z0-9\+\_\.\-]+)@([a-zA-Z0-9\.\-]+)\.([a-zA-Z]){1,7}$/
+let str = 'man6198u.gupta@gm.ai.l.com';
+
+let ans = regex.exec(str);
+console.log(ans);
+console.log(regex.test(str));
