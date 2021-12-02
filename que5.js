e9ce6370f0d6c29a7bc9e0960421936e7d18363b
coder biceps <manugupta619@gmail.com>
2021-12-02 18:29:52 +0530

    modify recursive function to take less space in stack

---
+++ que5.js
@@ -0,0 +1,22 @@
+// The following recursive code will cause a stack overflow if the array "somelist" is too large. 
+//How can you fix this and still retain the recursive pattern?
+
+// var somelist = readVeryLongList();
+// var nextItem = function() {
+//    var item = somelist.pop()
+//    if (item) {
+//        // process the list item...
+//        nextItem();
+//    }
+// };
+
+var somelist = readVeryLongList();
+function nextitem(idx , somelist){
+    if(idx<0)  return;
+   let ele = somelist[idx];
+   //do work with ele
+   return  nextitem(idx-1 , somelist) 
+}
+nextitem(somelist.length , somelist);
+
+
