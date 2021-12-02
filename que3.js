89e63006d6a5261054e1b45d88eaacc86f856e01
coder biceps <manugupta619@gmail.com>
2021-12-02 17:03:26 +0530

    add sleep function with promise that runs after x milliseconds

---
+++ que3.js
@@ -0,0 +1,14 @@
+//Using Promise create a function named 'sleep' that should invoke a callback function after x seconds. 
+//NOTE: sleep function should not block the call stack.
+
+function sleep(x){
+    return new Promise((resolve , reject)=>{
+         setTimeout(()=>{
+             resolve(`running after ${x} millisec`);
+         },x)
+    })
+}
+
+sleep(3000).then(res=>{
+    console.log(res);
+});
